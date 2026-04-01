10f5b3e84de53fbc936bdf42d0f2bd8ad210f450
yasmiinbianca <41231@raphaeldisanto.com.br>
2026-04-01 08:06:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import  Logo from './assets/Overwatch_logo.svg'
+
+export default function App() {
+
+  return (
+    <>
+    <main>
+      <header>
+        <img src="" alt="" />
+      </header>
+      <section></section>
+      <footer></footer>
+    </main>
+    </>
+  )
+}
